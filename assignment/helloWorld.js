3bb7dd8a6553ec1f435a6872c616eb7ba97b629e
Zach Wright <zwright101@gmail.com>
2023-06-10 11:44:51 -0500

    added fun facts, name, and image

---
+++ assignment/helloWorld.js
@@ -0,0 +1,33 @@
+console.log("hello")
+
+let x = 1;
+
+let tempRightNow = 78;
+
+tempRightNow = tempRightNow + 1;
+
+console.log("temp before too much Diablo" , tempRightNow);
+
+tempRightNow += 1;
+
+tempRightNow ++; 
+
+console.log(tempRightNow);
+let tempLater = '<p class="container">cool</p>';
+let game = "zelda";
+let title = "TotK"
+//let year = "2023";
+game = game + ' ' + title;
+console.log("game" , game);
+
+let isCloudy = false;    
+
+//let toReplace = document.querySelector('')
+
+//toReplace.innerHTML = tempLater;
+
+if (isCloudy === false) {
+    console.log('clear skies.');
+} else {
+    console.log('rainy day');
+}
